f309fbdf1f730cc00745463047d0efde627c993c
jaioneB <haplapmaster@gmail.com>
2024-09-26 11:13:32 +0200

    update bullet points

--- src/components/home/Publications.jsx
+++ src/components/home/Publications.jsx
@@ -1,53 +1,53 @@
 import React from 'react';
 import { Jumbotron } from "./migration";
 
-
 const Publications = ({ heading, message, resume }) => {
-    const publications = [
-      { title: "Research Paper 1", link: "#" },
-      { title: "Article 2", link: "#" },
-      { title: "Conference Paper 3", link: "#" },
-    ];
-  
-    return (
-      <Jumbotron id="publications" className="m-0">
-        <div className="container row">
-          <div className="col-lg-12"> {/* Full width column */}
-            <h2 className="display-4 mb-5 text-center">{heading}</h2>
-            <p className="lead text-center">{message}</p>
-            
-            <ul className="lead text-center list-unstyled d-flex flex-column align-items-center">
-              {publications.map((pub, index) => (
-                <li key={index} className="text-center">
-                  {pub.link ? (
-                    <a href={pub.link} className="publication-link">
-                      {pub.title}
-                    </a>
-                  ) : (
-                    <span>{pub.title}</span>
-                  )}
-                </li>
-              ))}
-            </ul>
-  
-            {resume && (
-              <p className="lead text-center">
-                <a
-                  className="btn btn-outline-dark btn-lg"
-                  href={resume}
-                  target="_blank"
-                  rel="noreferrer noopener"
-                  role="button"
-                  aria-label="Resume/CV"
-                >
-                  Resume
-                </a>
-              </p>
-            )}
-          </div>
+  const publications = [
+    { title: "Research Paper 1", link: "#" },
+    { title: "Article 2", link: "#" },
+    { title: "Conference Paper 3", link: "#" },
+  ];
+
+  return (
+    <Jumbotron id="publications" className="m-0">
+      <div className="container row">
+        <div className="col-lg-12"> {/* Full width column */}
+          <h2 className="display-4 mb-5 text-center">{heading}</h2>
+          <p className="lead text-center">{message}</p>
+
+          {/* Left-align and add bullet points */}
+          <ul className="lead list-unstyled">
+            {publications.map((pub, index) => (
+              <li key={index} className="text-left" style={{ listStyleType: "disc", marginLeft: "20px" }}>
+                {pub.link ? (
+                  <a href={pub.link} className="publication-link">
+                    {pub.title}
+                  </a>
+                ) : (
+                  <span>{pub.title}</span>
+                )}
+              </li>
+            ))}
+          </ul>
+
+          {resume && (
+            <p className="lead text-center">
+              <a
+                className="btn btn-outline-dark btn-lg"
+                href={resume}
+                target="_blank"
+                rel="noreferrer noopener"
+                role="button"
+                aria-label="Resume/CV"
+              >
+                Resume
+              </a>
+            </p>
+          )}
         </div>
-      </Jumbotron>
-    );
+      </div>
+    </Jumbotron>
+  );
 };
-  
+
 export default Publications;
